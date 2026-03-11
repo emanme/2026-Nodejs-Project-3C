313b7bb7d4cc32aac00ceaa721799cfe693f91c9
NicoTelen <nicochristiantelen@gmail.com>
2026-03-11 08:39:10 +0800

    Apply minimum password length check to login route

--- src/routes/users.js
+++ src/routes/users.js
@@ -21,7 +21,7 @@ const registerSchema = z.object({
 const loginSchema = z.object({
   body: z.object({
     email: z.string().email(),
-    password: z.string().min(1)
+    password: z.string().min(8, 'Password must be at least 8 characters')
   })
 });
 
