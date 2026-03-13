aab98f2143fed0a266b4706e6a81df88814bd66f
Christel <chrstl32@gmail.com>
2026-03-13 14:06:00 +0800

    Add API versioning prefix /api/v1 to all routes

--- src/app.js
+++ src/app.js
@@ -32,9 +32,9 @@ app.use((req, res, next) => {
 
 // ISSUE-0035: /health endpoint missing in release
 
-app.use('/users', users);
-app.use('/products', products);
-app.use('/orders', orders);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/orders', orderRoutes);
 
 // ISSUE-0016/0030: error handling inconsistent and stack logging not improved
 app.use((err, req, res, next) => {
